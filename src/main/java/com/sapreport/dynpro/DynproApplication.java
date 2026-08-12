255473a3ffd0b35205a8e78ab79b1bd651a1201d
Shubham Bansal <shubhambansal@TI-MAC-159.local>
2026-08-12 16:10:43 +0530

    initial commit

---
+++ src/main/java/com/sapreport/dynpro/DynproApplication.java
@@ -0,0 +1,13 @@
+package com.sapreport.dynpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapreport/dynpro/DynproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapreport.dynpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
